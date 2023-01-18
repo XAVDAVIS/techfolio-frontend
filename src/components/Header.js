a6d11878b5a92e6bc139ee35d91f90a6891ce7d0
Kaushik Mondal <kaushik@kaushiks-mbp.lan>
2023-01-17 20:42:32 -0500

    index page and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
 function Header(props){
-    return <h1>This is the Header Component.</h1>;
-  } 
+  return (
+    <nav>
+      <Link to='/'>
+        <li>Home</li>
+      </Link>
+      <Link to='/about'>
+        <li>About</li>
+      </Link>
+      <Link to='/portfolio/:id'>
+        <li>My Portfolio</li>
+      </Link>
+      <Link to='/explore'>
+        <li>Showcase Hub</li>
+      </Link>
+      <Link to='/login'>
+        <li>Log in</li>
+      </Link>
+      <Link to='/registration'>
+        <li>Sign up</li>
+      </Link>
+    </nav>
+  )
+} 
   
-  export default Header;
+export default Header;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
 function Index(props){
-    return <h1>This is the Home Page.</h1>;
-  } 
+  return(
+    <>
+      <h1>TechFolio</h1>
+      <p>A simple way to design a personal portfolio and showcase your brand with a network of like-minded career-seekers.</p>
+      <Link to='/signup'>
+        <button>Get Started</button>
+      </Link>
+      <section>
+        <img alt='portfolio page' src="https://images.unsplash.com/photo-1616036740257-9449ea1f6605?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3Vuc2V0JTIwYmVhY2h8ZW58MHx8MHx8&w=1000&q=80"/>
+        <h3>Create a Portfolio</h3>
+        <p>Style your own custom portfolio by adding a profile picture, links to your social media and GitHub. You can also showcase your projects and skills to show potential employers.</p>
+      </section>
+      <section>
+        <img alt='explore page' src="https://images.unsplash.com/photo-1616036740257-9449ea1f6605?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3Vuc2V0JTIwYmVhY2h8ZW58MHx8MHx8&w=1000&q=80"/>
+        <h3>Explore Other People's Portfolios</h3>
+        <p>Our app allows you to see other portfolios. Whether you're a recruiter or job seeker, the explore page will likely be useful to either.</p>
+      </section>
+    </>
+  )
+} 
   
-  export default Index;
+export default Index;
