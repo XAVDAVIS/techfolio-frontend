00093aa77cadeeb9c3d3f2a3039c2b460458a6df
Ariel <117614710+RielDreams@users.noreply.github.com>
2023-01-18 16:34:43 -0500

    backend explore page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import Explore from "../pages/Explore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Main(props) {
 	//Creating state for portfolio
 	const [portfolio, setPortfolio] = useState(null);
-	const url = "http://localhost:4000";
+	const url = "http://localhost:4000/portfolio/";
+
+	const getPortfolio = async () => {
+		const response = await fetch(url);
+		const data = await response.json();
+		setPortfolio(data);
+	};
+
+	useEffect(() => {
+		getPortfolio();
+	}, []);
 
 	return (
 		<main>
 			<Routes>
-				<Route path="/explore" element={<Explore />} />
+				<Route path="/explore" element={<Explore portfolio={portfolio} />} />
 			</Routes>
 		</main>
 	);
--- src/index.js
+++ src/index.js
@@ -8,21 +8,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-<<<<<<< HEAD
 	<React.StrictMode>
 		<Router>
 			<App />
 		</Router>
 	</React.StrictMode>,
 	document.getElementById("root")
-=======
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>,
-    document.getElementById("root")
-  </React.StrictMode>
->>>>>>> 1605afc7b9c0ece835f1ee49f4909807885bdc83
 );
 
 reportWebVitals();
